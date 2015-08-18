ba3a18e069b3f285fd4304bd798b07c70945fb23
Tim Suchanek <tim.suchanek@gmail.com>
2015-08-18 10:02:11 +0200

    Fixed STUN Server

--- src/settings.js
+++ src/settings.js
@@ -1,14 +1,14 @@
 module.exports = {
 	node: {
 		peerConnection: [
-		  { iceServers: [{url:'stun:turn.five.netsyno.com:3478'}] },
+		  { iceServers: [{url:'stun:stun.l.google.com:19302'}] },
 		  { optional: [{ DtlsSrtpKeyAgreement: true }] }
 		]
 	},
 	browser: {
 		peerConnection: {
 			config: {
-				iceServers: [{url:'stun:turn.five.netsyno.com:3478'}],
+                iceServers: [{url:'stun:stun.l.google.com:19302'}],
 				connection: {
 					optional: [{ DtlsSrtpKeyAgreement: true }]
 				}
